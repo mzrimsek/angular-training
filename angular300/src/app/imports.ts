1ae2f7fd616884a3e834f2ebafafb7d76f5da4ef
Mike Zrimsek <mikezrimsek@gmail.com>
2017-12-14 21:09:11 -0500

    Update imports.ts

--- angular300/src/app/imports.ts
+++ angular300/src/app/imports.ts
@@ -6,14 +6,15 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { CommonModule } from './common/common.module';
 import { MediaModule } from './features/media/media.module';
 import { GameModule } from './features/game/game.module';
 import { ShoppingModule } from './features/shopping/shopping.module';
+import { FormsStuffModule } from './features/forms-stuff/forms-stuff.module';
 
 import { reducers } from './reducers';
 import { environment } from '../environments/environment';
-import { FormsStuffModule } from './features/forms-stuff/forms-stuff.module';
 
 let imports = [
   BrowserModule,
